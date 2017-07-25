5e013c8ce8482fd55db1e5ecb0c13a286229df66
Jordan Vlieg <jordanvlieg@gmail.com>
2017-07-25 16:55:25 -0700

    Changed formatting of infopage

--- app/src/main/java/antidose/antidose/InformationActivity.java
+++ app/src/main/java/antidose/antidose/InformationActivity.java
@@ -1,6 +1,7 @@
 package antidose.antidose;
 
 import android.annotation.TargetApi;
+import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 public class InformationActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
@@ -18,7 +20,7 @@ public class InformationActivity extends AppCompatActivity implements TextToSpee
     private Button btnSpeak;
     private Button btnNext;
     private Button btnPrev;
-    private String[] steps = {"First Stimulate.  Rub your knuckles hard on the victims sternum.  Try to wake them. Call 911 if they are unresponsive.",
+    private String[] steps = {"First Stimulate.  Rub your knuckles hard on the victims sternum.  Try to wake them. Call nine one one if they are unresponsive.",
             "Ensure that the airway of the victim is clear and unobstructed.",
             "If the airway is clear, plug their nose, tilt their head back, and breathe for the victim.  Compressions are not necessary, simply provide air.  One breath every 5 seconds.",
             "Take a moment and evaluate the situation, is the person coming around?  Are they breathing alone?  They may need Naloxone.",
@@ -32,6 +34,11 @@ public class InformationActivity extends AppCompatActivity implements TextToSpee
         setContentView(R.layout.activity_information);
 
         tts = new TextToSpeech(this, this);
+        btnSpeak = (Button) findViewById(R.id.readSteps);
+        btnNext = (Button) findViewById(R.id.nextStep);
+        btnPrev = (Button) findViewById(R.id.prevStep);
+        btnNext.setVisibility(View.GONE);
+        btnPrev.setVisibility(View.GONE);
 
         tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
             @Override
@@ -56,22 +63,25 @@ public class InformationActivity extends AppCompatActivity implements TextToSpee
             }
         });
 
-        btnSpeak = (Button) findViewById(R.id.readSteps);
         btnSpeak.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 if(tts.isSpeaking()) {
                     btnSpeak.setText("Read Steps");
+                    btnNext.setVisibility(View.GONE);
+                    btnPrev.setVisibility(View.GONE);
                     tts.stop();
                 }
                 else {
                     btnSpeak.setText("Stop");
+                    btnNext.setVisibility(View.VISIBLE);
+                    btnPrev.setVisibility(View.VISIBLE);
                     startSpeech(currentStep = 0);
                 }
             }
         });
 
-        btnNext = (Button) findViewById(R.id.nextStep);
+
         btnNext.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -84,7 +94,6 @@ public class InformationActivity extends AppCompatActivity implements TextToSpee
                 }
             }
         });
-        btnPrev = (Button) findViewById(R.id.prevStep);
         btnPrev.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -105,32 +114,43 @@ public class InformationActivity extends AppCompatActivity implements TextToSpee
 
     }
 
-    @TargetApi(21)
     private void startSpeech(int firstStep){
-        if(android.os.Build.VERSION.SDK_INT >= 21) {
-            tts.speak("", TextToSpeech.QUEUE_FLUSH, null, null);
-            for (int theStep = firstStep; theStep < steps.length; theStep++) {
-                speak(steps[theStep], Integer.toString(theStep));
-            }
+        speak("", TextToSpeech.QUEUE_FLUSH, null);
+        for (int theStep = firstStep; theStep < steps.length; theStep++) {
+            speak(steps[theStep], TextToSpeech.QUEUE_ADD, Integer.toString(theStep));
+            speakSilent(2000,Integer.toString(theStep) + "silence");
         }
     }
 
-    @TargetApi(21)
-    public void speak(String text, String id){
+    @SuppressWarnings("deprecation")
+    public void speak(String text, int queueMode, String id){
         if(android.os.Build.VERSION.SDK_INT >= 21){
-            tts.speak(text, TextToSpeech.QUEUE_ADD, null, id);
-            tts.playSilentUtterance(2000, TextToSpeech.QUEUE_ADD, id+"silence");
+            tts.speak(text, queueMode, null, id);
+        }
+        else {
+            HashMap<String, String> map = new HashMap<>();
+            map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, id);
+            tts.speak(text, queueMode, map);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public void speakSilent(int length, String id){
+        if(android.os.Build.VERSION.SDK_INT >= 21){
+            tts.playSilentUtterance(length, TextToSpeech.QUEUE_ADD, id);
+        }
+        else {
+            HashMap<String, String> map = new HashMap<>();
+            map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, id);
+            tts.playSilence(length, TextToSpeech.QUEUE_ADD, map);
         }
     }
 
     @Override
     public void onInit(int status) {
-
         if (status == TextToSpeech.SUCCESS) {
-
             int result = tts.setLanguage(Locale.US);
             tts.setSpeechRate((float)0.9);
-
             if (result == TextToSpeech.LANG_MISSING_DATA
                     || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("TTS", "This Language is not supported");
